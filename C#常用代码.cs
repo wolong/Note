d993c30fd6598e3082e810c44a27c65082f25780
wolong <wolong001@163.com>
2017-08-13 00:04:28 +0800

    Update C#常用代码.cs

--- C#常用代码.cs
+++ C#常用代码.cs
@@ -1,1 +1,17 @@
 /*
+按下按键时
+KeyDown可为任何键
+KeyPress只是字母、数字、回车、Esc等
+*/
+
+//按下回车键
+
+private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+{
+    //按下 回车 且列表中只有一个时
+    if (e.KeyChar == (char)13)
+    {
+        //do something
+        e.Handled = true; //这段用于消除警告音
+    }
+}
